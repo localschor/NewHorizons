9703905ef49086716db1a095857bf01993628ab1
Jonathan Schor <localschor@gmail.com>
2015-11-12 02:56:13 +0200

    initial commit - haven't done anything yet

---
+++ game.py
@@ -0,0 +1,7 @@
+import character
+from nhdev import DevMode
+from nhscript import Script
+import nhitems
+import nhactions
+import nhworldmap
+
---
+++ protag.py
@@ -0,0 +1,4 @@
+class Character:
+    """ The base class for characters, including our Protag """
+    def __init__(self):
+        self.INVENTORY_SPACE = 
